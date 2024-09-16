106179110a1f4884dea104d9c96ad4d8277b0d62
josondev <163281456+josondev@users.noreply.github.com>
2024-09-16 16:19:07 +0530

    Update mainlyzr.py

--- lyzr project/mainlyzr.py
+++ lyzr project/mainlyzr.py
@@ -8,24 +8,18 @@ nest_asyncio.apply()
 class MAIN:
     def __init__(self, ans, key):
         self.ans = ans
-        self.__key = key
-        
-    def getkey(self):
-        return(self.__key)
-
-    def setter(self,key):
-        self.__key=key
+        self.key = key
         
     def main(self):
         if self.ans == 'voice':
             ans = input('Text-to-speech or Transcribe or Text-to-notes: ')
-            Voice(ans,self.__key)
+            Voice(ans,self.key)
         elif self.ans == 'summariser':
-            Summariser(self.__key)
+            Summariser(self.key)
         elif self.ans == 'generator':
-            Generator(self.__key)
+            Generator(self.key)
         else:
-            os.environ['OPENAI_API_KEY'] = self.__key
+            os.environ['OPENAI_API_KEY'] = self.key
             if self.ans == 'chat':
                 ans = input('word document or pdf or youtube video or website or webpage or text file: ')
                 Chat(ans)
@@ -41,7 +35,6 @@ if __name__ == '__main__':
             ans = input('voice or search or chat or generator or summariser: ')
             key = input('Enter your API key:')
             obj = MAIN(ans, key)
-            obj.setter(key)
             obj.main()
         except Exception as e:
             print("The following exception has occurred:", e)
